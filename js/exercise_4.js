c7be2d03ae1614ab0feb0e3f785ea956495a6c93
ddmurray06 <ddmurray06@comcast.net>
2016-05-02 13:16:06 -0600

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -25,7 +25,7 @@ var featureLayer = L.mapbox.featureLayer();
 
 featureLayer.on('ready', function(){
 	this.eachLayer(function(Layer){
-      	layer.setIcon(L.mapbox.marker.icon({
+      	Layer.setIcon(L.mapbox.marker.icon({
           	"marker-color": "#8834bb",
           	"marker-size": "large",
           	"marker-symbol": "restaurant"
