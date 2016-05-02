b75e935bd51ac4795dddbeefed9407114674d0b7
ddmurray06 <ddmurray06@comcast.net>
2016-05-02 14:03:32 -0600

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -40,3 +40,13 @@ featureLayer.on('ready', function(){
     layer.bindPopup('Welcome to ' + layer.feature.properties.name);
   });
 });
+
+
+#info {
+      position:absolute; top: 10px; right: 10px; bottom: 10px; width: 260px;
+      background:#333; color: #fff;
+      padding:20px;
+      font-family: Arial, Helvetica, sans-serif;
+      opacity:0.9;
+      filter:alpha(opacity=80); /* For IE8 and earlier */
+}
