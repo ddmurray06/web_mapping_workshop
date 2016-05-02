ea329dc5f3e42eb744547460bd306b7d874910dd
ddmurray06 <ddmurray06@comcast.net>
2016-05-02 14:38:00 -0600

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -95,3 +95,21 @@ map.on('click',function(e){
 });
 
 
+
+
+map.on('locationfound', function(e) {
+
+    myLocation.setGeoJSON({
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [e.latlng.lng, e.latlng.lat]
+        },
+        properties: {
+            'title': 'Here I am!',
+            'marker-color': '#ff8888',
+            'marker-symbol': 'star'
+        }
+    });
+
+});
