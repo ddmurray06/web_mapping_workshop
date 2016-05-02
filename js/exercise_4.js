3a68aa2ada2fc625bf5e822b4e2680343cc8ba03
ddmurray06 <ddmurray06@comcast.net>
2016-05-02 13:28:02 -0600

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -1,13 +1,38 @@
 -
+// Here is the javascript setup for a basic map:
 
-  
-  
-  
-  
-  
-  //exercise 6
+// Enter your mapbox map id here to reference it for the base layer,
+// this one references the ugly green map that I made.
+var mapId = 'ddmurray06.019h9jec';
+
+// And this is my access token, use yours.
+var accessToken = 'pk.eyJ1IjoiZGRtdXJyYXkwNiIsImEiOiJjaW5uaGxpZncwemxjdWtseWo0MXoybjNhIn0.ahMi6Zg3mhTE4sarB94T2g';
+
+// Create the map object with your mapId and token,
+// referencing the DOM element where you want the map to go.
+L.mapbox.accessToken = accessToken;
+var map = L.mapbox.map('map', mapId);
+
+// Set the initial view of the map to the whole US
+map.setView([39, -96], 4);
+
+// Great, now we have a basic web map!
+
+var dataFileToAdd = 'data/restaurants.geojson'
+
+var featureLayer = L.mapbox.featureLayer();
+	featureLayer.loadURL(dataFileToAdd);
+	featureLayer.addTo(map);
+
 featureLayer.on('ready', function(){
-  this.eachLayer(function(layer){
-    layer.bindPopup('Welcome to ' + layer.feature.properties.name);
-  });
-});
+	this.eachLayer(function(layer){
+      	layer.setIcon(L.mapbox.marker.icon({
+          	"marker-color": "#8834bb",
+          	"marker-size": "large",
+          	"marker-symbol": "restaurant"
+        }))
+    })
+    map.fitBounds(featureLayer.getBounds());
+})
+
+
